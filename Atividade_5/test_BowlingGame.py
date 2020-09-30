600d120459a8d96f0e25f8e071ed9849fb593516
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2020-09-29 21:38:06 -0300

    test: all one games

--- Atividade_5/test_BowlingGame.py
+++ Atividade_5/test_BowlingGame.py
@@ -1,8 +1,17 @@
-from BowlingGame import roll, score
+from BowlingGame import Game
 
 
 def test_score_0_gutter_game():
+    g = Game()
     for _ in range(20):
-        roll(0)
+        g.roll(0)
 
-    assert(score()) == 0
+    assert(g.get_score()) == 0
+
+
+def test_all_one_game():
+    g = Game()
+    for _ in range(20):
+        g.roll(1)
+
+    assert(g.get_score()) == 20
