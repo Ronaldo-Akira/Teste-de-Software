e970c4029d9ad8bcaade9ed3f86fac3b320bbc11
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2020-10-01 20:51:11 -0300

    Add tests bowling game and first mutation treated

---
+++ Atividade_6/test_BowlingGame.py
@@ -0,0 +1,55 @@
+from BowlingGame import Game
+
+
+def roll_many(pins, n, g):
+    for _ in range(n):
+        g.roll(pins)
+
+
+def test_bowling_initial_values():
+
+    g = Game()
+    assert(g.score()) == 0
+
+
+def test_score_0_gutter_game():
+    g = Game()
+    roll_many(0, 20, g)
+    assert(g.score()) == 0
+
+
+def test_all_one_game():
+    g = Game()
+    roll_many(1, 20, g)
+    assert(g.score()) == 20
+
+
+def test_spare():
+    g = Game()
+    roll_spare(g)
+    g.roll(3)
+    roll_many(0, 17, g)
+    assert(g.score()) == 16
+
+
+def test_strike():
+    g = Game()
+    roll_strike(g)
+    g.roll(5)
+    roll_many(0, 18, g)
+    assert(g.score()) == 20
+
+
+def test_perfect_game():
+    g = Game()
+    roll_many(10, 12, g)
+    assert(g.score()) == 300
+
+
+def roll_strike(g):
+    g.roll(10)
+
+
+def roll_spare(g):
+    g.roll(5)
+    g.roll(5)
